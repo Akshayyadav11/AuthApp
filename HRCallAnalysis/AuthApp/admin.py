43a15f1e739afb9f629f084d6e744d7f3937ec72
akshay yadav <akshayy976@gmail.com>
2025-08-01 13:43:56 +0530

    date format changed

--- HRCallAnalysis/AuthApp/admin.py
+++ HRCallAnalysis/AuthApp/admin.py
@@ -1,14 +1,50 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils import timezone
+from django.utils.html import format_html
 from .models import CustomUser, Role
 
+# Set the timezone to IST
+IST = timezone.get_fixed_timezone(330)  # UTC+5:30
+
+class IsDeletedFilter(admin.SimpleListFilter):
+    title = 'is deleted'
+    parameter_name = 'is_deleted'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'Yes'),
+            ('0', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '1':
+            return queryset.filter(is_deleted=True)
+        if self.value() == '0':
+            return queryset.filter(is_deleted=False)
+        return queryset
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'opo_id', 'mobile_no', 'role', 'is_active', 'is_staff', 'created_by')
-    list_filter = ('role', 'is_active', 'is_staff')
+    list_display = ('email', 'first_name', 'last_name', 'opo_id', 'mobile_no', 'role', 'is_active', 'is_staff', 'created_by', 'is_deleted', 'formatted_created_at', 'formatted_updated_at')
+    
+    def formatted_created_at(self, obj):
+        if obj.created_at:
+            return timezone.localtime(obj.created_at, timezone=IST).strftime('%d/%m/%Y %H:%M')
+        return "-"
+    formatted_created_at.short_description = 'Created At'
+    formatted_created_at.admin_order_field = 'created_at'
+    
+    def formatted_updated_at(self, obj):
+        if obj.updated_at:
+            return timezone.localtime(obj.updated_at, timezone=IST).strftime('%d/%m/%Y %H:%M')
+        return "-"
+    formatted_updated_at.short_description = 'Updated At'
+    formatted_updated_at.admin_order_field = 'updated_at'
+    list_filter = (IsDeletedFilter, 'role', 'is_active', 'is_staff', 'created_at', 'updated_at')
     search_fields = ('email', 'first_name', 'last_name', 'opo_id', 'mobile_no')
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
-    readonly_fields = ('created_at', 'updated_at', 'last_login', 'created_by')
+    readonly_fields = ('created_at', 'updated_at', 'last_login', 'created_by', 'is_deleted')
     
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
@@ -30,6 +66,30 @@ class CustomUserAdmin(UserAdmin):
         if not obj.pk:
             obj.created_by = request.user
         super().save_model(request, obj, form, change)
+    
+    def delete_queryset(self, request, queryset):
+        # Override delete_queryset to perform soft delete
+        for obj in queryset:
+            obj.is_deleted = True
+            obj.save()
+    
+    def delete_model(self, request, obj):
+        # Override delete_model to perform soft delete
+        obj.is_deleted = True
+        obj.save()
+    
+    def get_queryset(self, request):
+        # Get the base queryset
+        qs = super().get_queryset(request)
+        
+        # Check if we're filtering for deleted users
+        if request.GET.get('is_deleted') == '1':
+            return qs.filter(is_deleted=True)
+        elif request.GET.get('is_deleted') == '0':
+            return qs.filter(is_deleted=False)
+            
+        # By default, show all users
+        return qs
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Role)
